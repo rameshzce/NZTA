653bdb37c680a02311a284c97299ebfdb6ca6198
rameshkolamala <rameshzce@gmail.com>
2016-03-13 22:26:20 +0530

    design changes

--- app/src/main/java/com/tokkalo/nzta/SpecialAdapter2.java
+++ app/src/main/java/com/tokkalo/nzta/SpecialAdapter2.java
@@ -1,10 +1,14 @@
 package com.tokkalo.nzta;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -17,6 +21,7 @@ public class SpecialAdapter2 extends SimpleAdapter {
     private int[] colors = new int[] { 0x30FF0000, 0x300000FF };
 
     private int[] listItemBackground = new int[] { R.drawable.list_background3, R.drawable.list_background4 };
+    private int[] galleryBackground = new int[] { R.drawable.gallery_bg, R.drawable.gallery_bg2};
 
     public SpecialAdapter2(Context context, List<HashMap<String, String>> items, int resource, String[] from, int[] to) {
         super(context, items, resource, from, to);
@@ -41,15 +46,32 @@ public class SpecialAdapter2 extends SimpleAdapter {
         tv2.setTypeface(font);
 
         TextView tv3 = (TextView) view.findViewById(R.id.photoGallery);
+        String tvs3 = tv3.getText().toString();
 
         tv3.setTypeface(font);
 
         TextView tv4 = (TextView) view.findViewById(R.id.videoGallery);
-
+        String tvs4 = tv4.getText().toString();
         tv4.setTypeface(font);
+
+        TableRow tr = (TableRow) view.findViewById(R.id.tableRow1);
+
+        SpannableString content = new SpannableString(tvs3);
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+
+        SpannableString content2 = new SpannableString(tvs4);
+        content2.setSpan(new UnderlineSpan(), 0, content2.length(), 0);
+
 
         int colorPos = position % listItemBackground.length;
         view.setBackgroundResource(listItemBackground[colorPos]);
+        tr.setBackgroundResource(galleryBackground[colorPos]);
+        if(colorPos == 0){
+            tv3.setText(content);
+            tv4.setText(content2);
+        }
+
+
         //view.setLayoutParams(params);
         //view.setRight(100);
         return view;
